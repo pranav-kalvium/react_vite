056392cb153025ec3f6c7605beb7bdcf146a510c
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-02-08 12:39:01 +0530

    git home

---
+++ my-vite-app/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Welcome to My React App</h1>
+            <h2>Name: Pranav Prakash (SQ 70)</h2>
+            <h3>Contact: www.pranavprakash6410@gmail.com</h3>
+        </div>
+    );
+};
+
+export default Home;
---
+++ my-vite-app/src/main.jsx
@@ -0,0 +1,27 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
